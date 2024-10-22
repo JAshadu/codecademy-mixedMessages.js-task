056339cef31e8411fc259bd816f057007e64f6a7
JB_Ash <jashadu123@gmail.com>
2024-10-22 17:23:12 +0100

    First commit, added arrays

---
+++ mixedMessages.js
@@ -0,0 +1,19 @@
+// make an array of greetings
+const greeting = ['Hi', 'Bonjour', 'Hola', 'Guten Tag', 'Ciao', 'Namaste', 'Ni hao', 'Konnichi wa']
+// make an array of languages equal to the greetings
+const language = ['England', 'France', 'Spain', 'Germany', 'Italy', 'India', 'China', 'Japan']
+// make an array of names
+const name = ['Amanda', 'Benny', 'Charlie', 'Dennis', 'Elijah', 'Finland', 'Gerald', 'Hassan', 'Ijenu', 'Jacob', 'Karl', 'Liam', 'Mary', 'Nana', 'Oscar', 'Piper', 'Quincy', 'Raymond', 'Steven', 'Tilly', 'Unice', 'Victor', 'Wapol', 'Xavier', 'Yewande', 'Zack']
+// make an array of colors
+const color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet']
+// make an array of sports
+const sport = ['football','basketball', 'tennis', 'table tennis', 'swimming', 'volleyball', 'netball', 'hockey', 'badminton']
+// make an array of foods
+
+// loop through foods array, add if/else for most and least favourite
+
+// console 4 sentences that incorporate these items
+/*console.log(`${greeting}! My name is ${name}.`)
+console.log(`My favourite color is ${color}`)
+console.log('According to '+ name +', the sport I am best at is ' + sport + '.')
+console.log(`My favourite food is ${food} and my least favourite food is ${food}.`)*/
